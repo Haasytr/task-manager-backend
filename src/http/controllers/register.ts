7de66f4674a21f316cf7251789e089fc880bb936
Vinicius <viniciuszero@live.com>
2023-10-31 16:46:06 -0300

    Adding SOLID design patterns

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,36 +1,28 @@
-import { FastifyRequest, FastifyReply } from 'fastify'
-import { prisma } from "@/lib/prisma"
-import { z } from "zod"
-import { hash } from 'bcryptjs'
+import { type FastifyRequest, type FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { RegisterUseCase } from '@/use-cases/register'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 
 export async function register(req: FastifyRequest, reply: FastifyReply) {
-    const registerBodySchema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password: z.string().min(6)
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(req.body)
+
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
     })
+  } catch (err) {
+    reply.status(409).send()
+  }
 
-    const { name, email, password } = registerBodySchema.parse(req.body)
-
-    const userWithSameEmail = await prisma.user.findUnique({
-        where: {
-            email
-        }
-    })
-
-    if (userWithSameEmail) {
-        return reply.status(409).send()
-    }
-
-    const password_hash = await hash(password, 6)
-
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash,
-        }
-    })
-
-    return reply.status(201).send()
+  return await reply.status(201).send()
 }
